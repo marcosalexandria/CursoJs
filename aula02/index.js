6f4930d66f6e383aae6449b7038d0ad30f5c7563
Marcos Alexandria <marcos.alexandria01@gmail.com>
2024-05-26 17:14:19 -0300

    commit
    16. Exercícios - Const e Let

---
+++ aula02/index.js
@@ -0,0 +1,9 @@
+const nome = "Marcos";
+const sobreNome = "Alexandria";
+let idade = 26;
+let alturaEmM = 1.70;
+let pesoEmKg = 76;
+let imc = pesoEmKg / (alturaEmM * alturaEmM);
+
+console.log('nome: ', nome, ' ', sobreNome, ' sua idade: ', idade, ' sua altura: ', alturaEmM, ' seu IMC é: ', imc);
+console.log(`nome: ${nome} ${sobreNome}, idade: ${idade}, sua altura: ${alturaEmM}, seu IMC é: ${imc}`)
