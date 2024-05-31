733c3f77786c132cf3be19dce3914bced8936b70
Marcos Alexandria <marcos.alexandria01@gmail.com>
2024-05-31 10:45:12 -0300

    commit
    28. Exercícios com numbers e Math

---
+++ aula04/index.js
@@ -0,0 +1,14 @@
+let num = 9.355;
+let num1 = Math.floor(num); //arredonda pra baixo
+let num2 = Math.ceil(num);  //Arredonda pra cima
+let num3 = Math.round(num); //Arredonda para o mais próximo
+let num4 = Math.max(2, 55, 62.79, 1500, 322, -15);  //retona o maior valor
+let num5 = Math.min(2, 55, 62.79, 1500, 322, -15);  ////retona o menor valor
+let num6 = Math.floor(Math.random() * 11); //pegando um numero aleatório entre 0 e 10
+
+console.log(num1);
+console.log(num2);
+console.log(num3);
+console.log(num4);
+console.log(num5);
+console.log(num6);
